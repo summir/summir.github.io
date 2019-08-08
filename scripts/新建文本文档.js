11bfdd3dae6fffed392e38b53e7b7ab0444b2f94
summir <651595786@qq.com>
2019-08-09 01:24:17 +0800

    piking

--- scripts/新建文本文档.js
+++ scripts/新建文本文档.js
@@ -2,10 +2,10 @@ var spawn = require('child_process').exec;
 
 // Hexo 2.x 用户复制这段
 /*hexo.on('new', function(path){
-  spawn('start  D:\MarkdownPad2.exe ' + path);
+  spawn('start  "C:\Users\summit\AppData\Local\atom\atom.exe" ' + path);
 });*/
 
 // Hexo 3 用户复制这段
 hexo.on('new', function(data){
-  spawn('start   D:\MarkdownPad2.exe ' + data.path);
+  spawn('start   "C:\Users\summit\AppData\Local\atom\atom.exe" ' + data.path);
 });
--- source/tags/index.md
+++ source/tags/index.md
@@ -1,6 +1,5 @@
 ---
-title: "tags"
+title: 标签
 date: 2018-08-08 19:39:44
-type: tags
-layout: "tags"
+type: "tags"
 ---
